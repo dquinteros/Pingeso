ad053643b3e1457578f7bad9884f5b7f0996a0a2
Pingeso <Pingeso@Pingeso.localdomain>
2013-11-14 17:27:24 -0400

    prueba git

--- Amasys/Amasys-ejb/src/java/entity/Assistance.java
+++ Amasys/Amasys-ejb/src/java/entity/Assistance.java
@@ -6,12 +6,10 @@ package entity;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 
 /**
