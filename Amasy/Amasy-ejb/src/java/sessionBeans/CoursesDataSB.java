08f09ff65cd65f54ab966037db968f4c1a469628
dquinteros <daniel.quinterosc@usach.cl>
2013-12-10 10:00:09 -0400

    Mejoras interfaz

--- Amasy/Amasy-ejb/src/java/sessionBeans/CoursesDataSB.java
+++ Amasy/Amasy-ejb/src/java/sessionBeans/CoursesDataSB.java
@@ -4,10 +4,12 @@
  */
 package sessionBeans;
 
+import entity.BlockClass;
 import entity.Course;
 import entity.Teacher;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -29,13 +31,29 @@ public class CoursesDataSB implements CoursesDataSBLocal {
     // "Insert Code > Add Business Method")
 
     @Override
-    public ArrayList<Course> teacherCourses(long profesorActual) {
+    public ArrayList<Course> teacherCourses(long profesorActual) {        
         Collection<Course> res = em.find(Teacher.class, profesorActual).getListCourse();        
         ArrayList<Course> listaCursos = new ArrayList<> ();     
-        for (Course iter : res) {                                    
-            listaCursos.add(iter);                                               
+        for (Course iter : res) { 
+            if(getIdBloackClassForTakeAttendance(iter.getId())!=null){
+                listaCursos.add(iter);                          
+            }                                 
         }
         return listaCursos;
     }
+    
+    private BlockClass getIdBloackClassForTakeAttendance(Long course) {
+        Date date = new Date();        
+        Collection<BlockClass> res = em.find(Course.class, course).getListBlockClass(); 
+        if(res.isEmpty()){
+            return null;
+        }
+        for (BlockClass iter : res) {
+            if( iter.getDate().getTime()/1000 -5*60 <= date.getTime()/1000 && date.getTime()/1000 <= iter.getDate().getTime()/1000+30*60){
+                return iter;
+            }
+        }
+        return null;
+    } 
 
 }
--- Amasy/Amasy-web/src/java/TableBean.java
+++ Amasy/Amasy-web/src/java/TableBean.java
@@ -1,93 +1,88 @@
 
-  
-import java.io.Serializable;  
-import java.util.ArrayList;  
-import java.util.List;  
-import java.util.UUID; 
-  
-public class TableBean implements Serializable {  
-  
-    private final static String[] colors;  
-  
-    private final static String[] manufacturers;  
-  
-    static {  
-        colors = new String[10];  
-        colors[0] = "Black";  
-        colors[1] = "White";  
-        colors[2] = "Green";  
-        colors[3] = "Red";  
-        colors[4] = "Blue";  
-        colors[5] = "Orange";  
-        colors[6] = "Silver";  
-        colors[7] = "Yellow";  
-        colors[8] = "Brown";  
-        colors[9] = "Maroon";  
-  
-        manufacturers = new String[10];  
-        manufacturers[0] = "Mercedes";  
-        manufacturers[1] = "BMW";  
-        manufacturers[2] = "Volvo";  
-        manufacturers[3] = "Audi";  
-        manufacturers[4] = "Renault";  
-        manufacturers[5] = "Opel";  
-        manufacturers[6] = "Volkswagen";  
-        manufacturers[7] = "Chrysler";  
-        manufacturers[8] = "Ferrari";  
-        manufacturers[9] = "Ford";  
-    }  
-  
-    private List<Car> filteredCars;  
-  
-    private List<Car> cars;  
-  
-    private Car selectedCar;  
-  
-    private Car[] selectedCars;  
-  
-    public TableBean() {  
-        cars = new ArrayList<Car>();            
-        populateRandomCars(cars, 50);  
-    }  
-  
-    public Car getSelectedCar() {  
-        return selectedCar;  
-    }  
-  
-    public void setSelectedCar(Car selectedCar) {  
-        this.selectedCar = selectedCar;  
-    }  
-  
-    private void populateRandomCars(List<Car> list, int size) {  
-        for(int i = 0 ; i < size ; i++)  
-            list.add(new Car(getRandomModel(), getRandomYear(), getRandomManufacturer(), getRandomColor()));  
-    }  
-  
-    private int getRandomYear() {  
-        return (int) (Math.random() * 50 + 1960);  
-    }  
-  
-    private String getRandomColor() {  
-        return colors[(int) (Math.random() * 10)];  
-    }  
-  
-    private String getRandomManufacturer() {  
-        return manufacturers[(int) (Math.random() * 10)];  
-    }  
-  
-    private String getRandomModel() {  
-        return UUID.randomUUID().toString().substring(0, 8);  
-    }  
-  
-    public List<Car> getFilteredCars() {  
-        return filteredCars;  
-    }  
-  
-    public void setFilteredCars(List<Car> filteredCars) {  
-        this.filteredCars = filteredCars;  
-    }  
-  
-    public List<Car> getCars() {  
-        return cars;  
-    }  
-}  
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class TableBean implements Serializable {
+
+    private final static String[] colors;
+    private final static String[] manufacturers;
+
+    static {
+        colors = new String[10];
+        colors[0] = "Black";
+        colors[1] = "White";
+        colors[2] = "Green";
+        colors[3] = "Red";
+        colors[4] = "Blue";
+        colors[5] = "Orange";
+        colors[6] = "Silver";
+        colors[7] = "Yellow";
+        colors[8] = "Brown";
+        colors[9] = "Maroon";
+
+        manufacturers = new String[10];
+        manufacturers[0] = "Mercedes";
+        manufacturers[1] = "BMW";
+        manufacturers[2] = "Volvo";
+        manufacturers[3] = "Audi";
+        manufacturers[4] = "Renault";
+        manufacturers[5] = "Opel";
+        manufacturers[6] = "Volkswagen";
+        manufacturers[7] = "Chrysler";
+        manufacturers[8] = "Ferrari";
+        manufacturers[9] = "Ford";
+    }
+    private List<Car> filteredCars;
+    private List<Car> cars;
+    private Car selectedCar;
+    private Car[] selectedCars;
+
+    public TableBean() {
+        cars = new ArrayList<Car>();
+        populateRandomCars(cars, 50);
+    }
+
+    public Car getSelectedCar() {
+        return selectedCar;
+    }
+
+    public void setSelectedCar(Car selectedCar) {
+        this.selectedCar = selectedCar;
+    }
+
+    private void populateRandomCars(List<Car> list, int size) {
+        for (int i = 0; i < size; i++) {
+            list.add(new Car(getRandomModel(), getRandomYear(), getRandomManufacturer(), getRandomColor()));
+        }
+    }
+
+    private int getRandomYear() {
+        return (int) (Math.random() * 50 + 1960);
+    }
+
+    private String getRandomColor() {
+        return colors[(int) (Math.random() * 10)];
+    }
+
+    private String getRandomManufacturer() {
+        return manufacturers[(int) (Math.random() * 10)];
+    }
+
+    private String getRandomModel() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public List<Car> getFilteredCars() {
+        return filteredCars;
+    }
+
+    public void setFilteredCars(List<Car> filteredCars) {
+        this.filteredCars = filteredCars;
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
