43364522579d9dd26ee7f5e8b3fe5ea751f34c64
Mauricio Oyarzún <mauricio2141@gmail.com>
2013-12-25 23:21:07 -0400

    Solucionando agregar admin...

--- Amasy/Amasy-ejb/src/java/entity/Admin.java
+++ Amasy/Amasy-ejb/src/java/entity/Admin.java
@@ -52,6 +52,42 @@ public class Admin {
         this.user = user;
     }
     
-    
+    /**
+     *
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    /**
+     *
+     * @param object
+     * @return
+     */
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Admin)) {
+            return false;
+        }
+        Admin other = (Admin) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "entity.Admin[ id=" + id + " ]";
+    }
     
 }
